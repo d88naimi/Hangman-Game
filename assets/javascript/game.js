96a288b183a439cf05ebced30de443d33d0e2e2c
David Naimi <d88naimi@gmail.com>
2017-01-30 20:33:31 -0800

    changed to psychic game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,47 +1,99 @@
-//array
+//array for random generate a letter
  var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
         'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
         't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
- 	 
-      // var wins = 0;
-      // var losses = 0;
+// objects---used to define the properties for the game  
+var psychic = {
+	// wins,losses life set default to 0 only once to get started
+	wins: 0,
+	losses: 0,
+	life: 10,
+	// array for future userGuess incorrect inputs intializing
+	incorrect: [],
+	// generating a random letter from array alphabet
+	computerGuess: alphabet[Math.floor(Math.random() * alphabet.length)],
+	// method-- resetting the game but not affecting the win and losses 
+	startover: function(){
+		this.life=10;
+		this.incorrect=[];
+		this.computerGuess= alphabet[Math.floor(Math.random() * alphabet.length)];
+		console.log(this.computerGuess);
+	}
+}
+
+
+
       
 
-      // document.onkeyup = function(event) {
 
-var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
 console.log(alphabet);
 
 
- console.log(computerGuess);
+ console.log(psychic.computerGuess);
 
 
 
-var guess= prompt("Guess a letter, or click cancel to stop playing");
-// prompt takes guess from player break;???
-	if (guess===null) {
+// holding the users key input
+document.onkeyup = function(event) {
+
+       // asssihning users input to userGuess 
+	var userGuess = event.key;
+    // javascript pre-set code for letters of the alphabet on a computer keyboard
+    if (event.keyCode >= 65 && event.keyCode <= 90){
+    // indexOf is telling computer that userGuess has been logged already resulting in false
+    // will not continue running	
+    	if (psychic.incorrect.indexOf(userGuess) == -1){ 
+
+
+
+
+
+    		// if userguess = random computer guess 
+			if (userGuess == psychic.computerGuess){
+				// add +1 to score
+				psychic.wins++
+				// function to reset
+				psychic.startover();
+
+				console.log("correct");
+
+			}	
+    
+			else { 
+				console.log ("Incorrect");
+				// minus a life
+				psychic.life--
+				// logging the incorrect key press to incorrect display
+				psychic.incorrect.push(userGuess);
+				// if all lives go to 0 
+				if (psychic.life == 0){
+					// add 1 to loses
+					psychic.losses++
+					// function to reset new game
+					psychic.startover();	
+				}
+
+			} 
+			// assighnments are triggered if valid keys are inputed
+    		document.getElementById('wins').value = psychic.wins;
+    		document.getElementById('losses').value = psychic.losses;
+    		document.getElementById('life').value = psychic.life;
+    		document.getElementById('incorrect').value = psychic.incorrect;
+    	}
+    }
+}
+
+
+
+	
+
+
+	
+
+	 
+
 		
-	}
-// checking if guess = one letter
-
-	else if (guess.length !==1){
-		alert("Please enter single letter")
-
-	}
-
-	  else if ( guess == computerGuess){
-		console.log(guess + " Great Job ");
-	
-	}
-
-	else if (guess!== computerGuess){
-		alert( guess + " Sorry Incorrect")
-	} 
-
-	
-
-	wins=( guess+ 1)	
 	
 
 	
